5becd3b31d040197cf83fe095b1e980cb74c708e
Niyas <niyas@Niyass-MacBook-Air.local>
2024-07-15 03:05:23 -0700

    latest

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,15 @@
+import Cover from "../about-us/Cover";
+import AboutLanding from "../../app/about-us/AboutLanding";
+import AboutSecondSection from "../about-us/AboutSecondSection"
+
+function page() {
+  return (
+    <>
+      <Cover />
+      <AboutLanding />
+      {/* <AboutSecondSection/> */}
+    </>
+  );
+}
+
+export default page;
