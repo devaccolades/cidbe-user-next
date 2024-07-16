005cfefdb47d7b3470312670ac69324a76793cae
sifan <sifan007sifu@gmail.com>
2024-07-16 09:47:15 +0530

    add the about landing page

--- src/app/about-us/AboutLanding.jsx
+++ src/app/about-us/AboutLanding.jsx
@@ -1,32 +1,30 @@
 import Image from "next/image";
 import shape from "../../../public/images/about/about-banner-shape.svg";
-import bannerRightImage from "/Users/niyas/Desktop/Cidbi/cidbe-user-next/public/images/home/about-image.jpg"
+import shapeMobile from "../../../public/images/about/about-banner-shape-mobile.svg";
+import bannerRightImage from "../../../public/images/home/about-image.jpg"
 import bannerLogo from "../../../public/images/about/about-us-cidbi-logo.svg"
 
 export default function AboutLanding() {
   return (
     <>
-    <section className="h-[350px] lg:h-[760px]  flex flex-row rounded-br-[120px] -mt-[80px] lg:-mt-[95px] bg-[#EDFBFD]">
-      <div className="w-[60%] flex items-center justify-center">
-      <div className="w-[710px] absolute left-[290px] top-[224]">
-        <Image src={bannerLogo} alt="bannerLogo" className="h-[207px] w-[430px]" />
-         <h1 className="w-[680px]  mt-10 font-[general-sans-regular] text-[#483C32] text-[40px]  uppercase leading-[54px]">Your premier choice for <span className="text-[#00652E]"> quality </span> housing solutions in <span className="text-[#00652E]">Thrissur</span>  </h1>
+      <section className="h-[720px] md:h-[350px] overflow-hidden main-section lg:h-[760px] flex flex-col md:flex-row rounded-br-[60px] md:rounded-br-[60px] lg:rounded-br-[120px] -mt-[80px] lg:-mt-[95px] bg-[#EDFBFD]">
+        <div className="flex justify-center items-end md:items-center w-full -mt-[30px] mb-[20px] md:mb-0 md:mt-0 lg:mt-[70px] h-[360px] md:h-full">
+          <div className="flex justify-center  flex-col gap-[20px] ms-[5%] md:ms-[120px]">
+          <Image src={bannerLogo} alt="bannerLogo" className="w-[187px] md:w-[22.39vw]" />
+          <h1 className="w-10/12 lg:w-[35.4wv] lg:max-w-[680px] font-[general-sans-regular] text-[#483C32] text-[20px] lg:text-[40px]  uppercase leading-[27px] lg:leading-[54px]">Your premier choice for <span className="text-[#00652E]"> quality </span> housing solutions in <span className="text-[#00652E]">Thrissur</span>  </h1>
+          </div>
         </div>
-      </div>
-      <Image
-  src={shape}
-  alt="Shape"
-  className="w-[40px] z-10   "
-  style={{ height: 'fit-content', objectFit: 'contain' }}
-/>
-      <div className="w-[40%] h-full bg-cover bg-center -ms-[40px] rounded-br-[120px] " style={{ backgroundImage: `url(${bannerRightImage.src})` }}/>
-    </section>
-    <div className="h-screen">
+        <Image
+          src={shape}
+          alt="Shape"
+          className="w-[40px] z-10 -ms-[20px] -me-[40px] shape hidden md:block"
+          style={{ height: 'fit-content', objectFit: 'contain' }}
+        />
+        <Image src={shapeMobile} className="minw-[400px] w-full z-10 mt-[20px] -mb-[60px] md:hidden block"/>
+        <div className="w-full md:w-8/12 h-[360px] md:h-full bg-cover bg-center rounded-br-[60px] md:rounded-br-[60px] lg:rounded-br-[120px] " style={{ backgroundImage: `url(${bannerRightImage.src})` }} />
+      </section>
+    </>
 
-    </div>
-  </>
-  
-    )
-    }
-   
-    
+  )
+}
+
--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,7 +1,7 @@
 import Cover from "../about-us/Cover";
 import AboutLanding from "../../app/about-us/AboutLanding";
 import AboutSecondSection from "../about-us/AboutSecondSection"
-
+import './About.css'
 function page() {
   return (
     <>
