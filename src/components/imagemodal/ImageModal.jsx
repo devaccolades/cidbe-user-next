f774d6635a0c33dece0678b8fff90cd8f1115c60
sifan <sifan007sifu@gmail.com>
2024-07-31 09:30:12 +0530

    image modal completed

--- src/components/imagemodal/ImageModal.jsx
+++ src/components/imagemodal/ImageModal.jsx
@@ -9,7 +9,7 @@ import {
 function ImageModal({ open, handleOpen ,data}) {
   return (
     <>
-      <Dialog className="relative bg-white shadow-none" open={open} handler={handleOpen}>
+      <Dialog className="relative bg-transparent shadow-none" open={open} handler={handleOpen}>
         <p className="absolute border-[2px] text-2xl rounded-full right-3 top-3 border-[--secondary-cl] text-[--secondary-cl] cursor-pointer font-bold px-[9px]" onClick={handleOpen}>x</p>
           <div className="h-[90vh] bg-contain rounded-[16px] bg-no-repeat bg-center" style={{ backgroundImage: `url(${data})` }} />
       </Dialog>
