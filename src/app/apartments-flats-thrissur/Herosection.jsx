362987a09dbacef395cbf7d90e4609f24ccd90d0
aswin <aswink.accolades@gmail.com>
2025-08-05 13:57:58 +0530

    schema, canonical and video fix

--- src/app/apartments-flats-thrissur/Herosection.jsx
+++ src/app/apartments-flats-thrissur/Herosection.jsx
@@ -5,7 +5,7 @@ import Herobottom from "./Herobottom";
 
 export default function Herosection() {
   return (
-    <section className="relative -mt-[78px] lg:-mt-[95px] text-white w-full h-full">
+    <section id="hero" className="relative -mt-[78px] lg:-mt-[95px] text-white w-full h-full">
       {/* Aspect ratio wrapper */}
       <div className="relative w-full sm:aspect-[12/16] aspect-[16/9] h-[85vh] xs:hidden">
         <video
@@ -17,7 +17,17 @@ export default function Herosection() {
           className="w-full h-full object-cover"
         ></video>
       </div>
-      <div className="hidden xs:block relative w-full ">
+       <div className="hidden xs:block relative w-full ">
+        <video
+          src="/video/tsr/CIDBI_l.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="object-cover object-center w-full"
+        />
+      </div>
+      <div className="hidden">
         <video
           src="/video/tsr/CIDBI_l.mp4"
           autoPlay
