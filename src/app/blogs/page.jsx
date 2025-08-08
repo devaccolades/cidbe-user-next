b5ca10fcbc4da68f737d4aff21fa705db6451e1d
aswin <aswink.accolades@gmail.com>
2025-08-08 12:58:20 +0530

    schema in blogs page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -5,6 +5,7 @@ import Footer from '../../layout/Footer';
 import Skelten from '../../components/skeletoneffect/Skelten';
 import { getBlogsApi, getSeoApi } from '../../services/services';
 import './Blogs.css';
+import Script from 'next/script';
 
 const BlogListing = dynamic(() => import('./BlogListing'),
  { ssr: false, 
@@ -28,6 +29,32 @@ export async function generateMetadata() {
       description: 'Default Description',
     };
   }
+}
+
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "BlogPosting",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://cidbi.com/blogs/2-bhk-flat-in-thrissur-the-perfect-choice"
+  },
+  "headline": "2 BHK Flat in Thrissur: Perfect Choice for Small Families",
+  "image": "https://cidbi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F8.7a355fa9.webp&w=3840&q=75",  
+  "author": {
+    "@type": "Organization",
+    "name": "Cidbi Builders",
+    "url": "https://cidbi.com/"
+  },  
+  "publisher": {
+    "@type": "Organization",
+    "name": "Cidbi",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://cidbi.com/_next/static/media/logo.5484c0aa.svg"
+    }
+  },
+  "datePublished": "2023-07-25"
 }
 
 export default async function Page({ searchParams }) {
@@ -55,6 +82,11 @@ export default async function Page({ searchParams }) {
       <Header bgPrimary={true} />
       <BlogListing data={blogs} totalCount={totalCount} currentPage={currentPage} pageSize={pageSize} />
       <Footer backGround='#FFFFFF' />
+      <Script
+        id="seo-json-ld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
     </>
   )
 }
